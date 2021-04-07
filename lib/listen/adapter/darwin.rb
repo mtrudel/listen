513f8012265883443610092d0e884cc1d90b8461
Mat Trudel <mat@geeky.net>
2021-04-07 12:31:55 -0400

    Manually patch https://github.com/guard/listen/pull/479

--- lib/listen/adapter/darwin.rb
+++ lib/listen/adapter/darwin.rb
@@ -6,7 +6,7 @@ module Listen
     # Adapter implementation for Mac OS X `FSEvents`.
     #
     class Darwin < Base
-      OS_REGEXP = /darwin(1.+)?$/i
+      OS_REGEXP = /darwin(?<major_version>(1|2)\d+)/i
 
       # The default delay between checking for changes.
       DEFAULTS = { latency: 0.1 }
